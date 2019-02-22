d5146fb8f469f4d23d4af9912975b24377c83d59
ConstantinePetrash <pca173@yahoo.com>
2019-02-22 12:43:48 +0200

    test2

--- src/application/repository/EventRepo.java
+++ src/application/repository/EventRepo.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import application.entity.Event;
 
 public interface EventRepo extends JpaRepository<Event, Long>{
+	//!!!!!!!!!!!!!!
 
 }
