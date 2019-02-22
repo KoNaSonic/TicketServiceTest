d24648fe08de1b43ce934d6a699b61eb39652f19
KoNaSonic <cona2002@gmail.com>
2019-02-22 18:44:13 +0200

    edit package repo

--- src/application/repository/EventJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import application.entity.EventEntity;
-
-public interface EventJpaRepository extends JpaRepository<EventEntity, Integer>{
-
-}
