3e0e97a6c4ce97ccb677a086ce77269f20792d71
Max Berenshteyn <shteynumaks@gmail.com>
2019-02-24 15:19:21 +0200

    I fixed and added some methods

--- src/application/model/AdminNotManager.java
+++ src/application/model/AdminNotManager.java
@@ -1,28 +1,61 @@
 package application.model;
 
+import application.entity.EventEntity;
+import application.entity.HallEntity;
+import application.entity.TicketEntity;
+
+import java.util.Date;
+import java.util.List;
+
 public interface AdminNotManager {
 
     public boolean addUser(String email, String password);
     // Manually add user to the system. For a example account of a new manager or cashier;
-    public boolean addRole(User user);
-    // Add new role to the system;
-    public User givingRole(Role role);
-    // Assignment role to User;
-    public Role editRole(Role role);
+
+    public User addRole(String userEmail, Enum role); // userEmail it's a primary key, list of a roles contained in a class Enum;
+    // Add role to the User;
+
+    public Enum editRole(Enum role);
     // Edit existing role for the specific functionality; It is need to find out how to fix accesses of roles with a help of methods
     // not security.
+
     public boolean deleteRole();
     public boolean deleteUser();
-    public String getPassword(User user);
-    //Get password of specific account if user don't success restore an account;
-    public String [] getRoles (User user);
-    //Get array of all roles in a system;
-    public String [] getUser (Role role);
-    public boolean addHole(String nameHall);
-    //Add new hall to the system;
-    public Hall editHall(Hall hall);
-    // Edit hall, for a example quantity of seats, hall type and etc.;
-    public Hall deleteHall(Hall hall);
 
+    public String getPassword(String userEmail);
+    //Get password of specific account if user don't success restore an account;
+
+    public String [] getUser (Enum role);
+
+    public boolean addHole(String hallName, String hallType, int seatsTotal, String layout);
+    //Add new hall to the system;
+
+    public HallEntity editHall(int hallId, int seatId);
+    // Edit hall, for a example quantity of seats, hall type and etc.;
+
+    public HallEntity deleteHall(int hallId);
+
+    public TicketEntity soldTicketsByDate(Date date);
+    // Show sold tickets by specific day;
+
+    public TicketEntity soldTicketsByHall(int hallId);
+    // Show sold tickets by specific day;
+
+    public Integer sumOfPaymentsByHall(int hallId);
+    // Show sum of sold tickets by Hall;
+
+    public Integer sumOfPaymentsByDate(Date date);
+    // Show sum of sold tickets by Date;
+
+    public Integer sumOfPaymentsByUser(String iserEmail);
+    // Show sum of sold tickets by User;
+
+    public List<EventEntity> eventsByMonth(Date date);
+    // Show all of events by month;
+
+    public String addNews(String news);
+    // Add news to the site;
 
 }
+
+
